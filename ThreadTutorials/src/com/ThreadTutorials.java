229f03a948f9ec6b2d41c3e110164e4c025abf8d
imsnehap <snehap.1789@gmail.com>
2023-09-18 18:24:46 +0530

    Test

---
+++ ThreadTutorials/src/com/ThreadTutorials.java
@@ -0,0 +1,11 @@
+package com;
+
+public class ThreadTutorials {
+	
+  public static void main(String args[]) {
+	  
+	  int core = Runtime.getRuntime().availableProcessors();
+	  System.out.print("Test>>>>"+core);
+  }
+
+}
---
+++ ThreadTutorials/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author snehap
+ *
+ */
+module ThreadTutorials {
+}
